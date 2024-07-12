2ce4227ae1901457c6b320b555baac0586ea0847
Travers Pinkerton <jtpinkerton@gmail.com>
2024-07-12 15:56:43 +0000

    remove vercel analytics due to failure

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 import React from "react";
 import App from "next/app";
 import "../css/tailwind.css";
-import { Analytics } from "@vercel/analytics/react";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Component {...pageProps} />;
-        <Analytics />
-      </>
-    );
+    return <Component {...pageProps} />;
   }
 }
 
